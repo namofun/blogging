f8c61d1513707fb0375258ea2a9ff5c8088e10ae
小羊 <tlylz99@live.cn>
2020-09-19 17:17:18 +0800

    Initialize the repository.

---
+++ src/Extensions.BloggingModule/BloggingModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SatelliteSite.BloggingModule
+{
+    public class BloggingModule<TContext> : AbstractModule where TContext : DbContext
+    {
+        public override string Area => "Blog";
+
+        public override void Initialize()
+        {
+        }
+    }
+}
